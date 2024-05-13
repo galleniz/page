2f81a817fdd18641a1138f156ee11884e0ea9168
xXMomazosNiz_69Xx <76523353+galleniz@users.noreply.github.com>
2024-05-13 11:29:22 -0600

    Update main.js

--- coco-page/main.js
+++ coco-page/main.js
@@ -1,6 +1,6 @@
 const params = Object.fromEntries((new URLSearchParams(window.location.search)).entries());
 if (params.isalt != null && params.isalt === "true")
-    document.write("<img class=img src=img/alt/"+ Math.floor(Math.random() * 2)  +".jpg>");
+    document.write(`<img class=img src="../img/alt/`+ Math.floor(Math.random() * 2)  +".jpg\">");
 else
-    document.write("<img class=img src=img/coco"+ Math.floor(Math.random() * 5)  +".jpg>");
+    document.write("<img class=img src=\"../img/coco"+ Math.floor(Math.random() * 5)  +".jpg\">");
 console.log("Cocos-random-img-engine v2")
